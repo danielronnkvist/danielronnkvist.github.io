0d54d438563a1af98631c52a0ff978e8b94ca712
danielronnkvist <daniel@ronnkvist.com>
2014-10-28 21:54:14 +0100

    Added flickr pictures to a new section of the site

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var module = angular.module('trevligheten', [], function($interpolateProvider) {
-    $interpolateProvider.startSymbol('//');
-    $interpolateProvider.endSymbol('//');
+    $interpolateProvider.startSymbol('/*/');
+    $interpolateProvider.endSymbol('/*/');
 });
 
 module.controller('insta', function ($scope, Instagram) {
   var user = '326924' // trevligheten
 
   Instagram.get(8, user).success(function(response){
-    $scope.images = response.data
+    $scope.images = response.data;
   })
 });
 
@@ -32,3 +32,19 @@ module.factory('Instagram', ['$http',
     };
   }
 ]);
+
+module.controller('flickr', function ($scope, Flickr) {
+  Flickr.get().success(function (response){
+    $scope.images = response.photos.photo;
+  });
+});
+
+module.factory('Flickr', ['$http', function ($http) {
+  // var url = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=328531f8cc4cb07d65e1a4316c8c5a1d&user_id=48988818%40N08&format=json"
+  var url = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=328531f8cc4cb07d65e1a4316c8c5a1d&user_id=48988818%40N08&format=json&nojsoncallback=1";
+  return{
+    'get': function() {
+      return $http.get(url);
+    }
+  };
+}]);
