ea0d907f26fd17775c4a75c58a45bda1b231171b
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-09-30 23:44:15 +0200

    Display body if present§

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import './Header.scss';
 
 export default class Header extends Component {
+
+  displayBody(body) {
+    if(body && typeof body === 'string') {
+      return (
+        <p>
+          {body}
+        </p>
+      );
+    }
+  }
+
   render() {
     const {
       title,
       ingress,
+      body,
     } = this.props;
 
     return (
       <div className="header">
         <h1>{title}</h1>
         <h4>{ingress}</h4>
+        { this.displayBody(body) }
       </div>
     );
   }
