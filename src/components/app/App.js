1968549b56d8ed5b15cd391180efecd435687824
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-09-30 23:43:36 +0200

    Display an example collection of projects

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,19 @@ import './App.scss';
 
 // Components
 import Header from './../header/Header.js';
+import Project from './../project/Project.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header title="trevligheten" ingress="daniel rönnkvist"/>
+        <div className="projects">
+          <Project title="Stereoscopic Snow Simulation"/>
+          <Project title="Stereoscopic Snow Simulation"/>
+          <Project title="Stereoscopic Snow Simulation"/>
+          <Project title="Stereoscopic Snow Simulation"/>
+        </div>
       </div>
     );
   }
