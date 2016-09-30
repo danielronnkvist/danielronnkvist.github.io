a7681673511bb180a5a2cedce3c85dfe7c3ef5bf
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-09-30 19:46:16 +0200

    Create an app component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app/App';
 
 ReactDOM.render(
-  <h1>hello world</h1>,
+  <App/>,
   document.getElementById('app')
 );
