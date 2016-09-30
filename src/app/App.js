c519e617cd70a68679833f50e46138e96c500ba7
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-09-30 21:37:00 +0200

    Simple header placeholder

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+// Components
+import Header from './../header/Header.js';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Header/>
       </div>
     );
   }
---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>trevligheten</h1>
+        <h4>Daniel Rönnkvist</h4>
+      </div>
+    );
+  }
+}
