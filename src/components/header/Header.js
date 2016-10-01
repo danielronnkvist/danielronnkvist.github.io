fce114c6084a8ddd135a3b68b0ec47d0ede8a1f9
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-10-01 11:20:02 +0200

    Animate in the projects

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './Header.scss';
 
 export default class Header extends Component {
@@ -25,10 +26,17 @@ export default class Header extends Component {
     };
 
     return (
-      <div className="header" style={headerStyle}>
-        <h1>{title}</h1>
-        <h4>{ingress}</h4>
-        { this.displayBody(html) }
+      <div className="header">
+        <ReactCSSTransitionGroup
+          transitionName="header-animation"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          <div key={image} className="background" style={headerStyle}>
+            <h1>{title}</h1>
+            <h4>{ingress}</h4>
+            { this.displayBody(html) }
+          </div>
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
