262955dd85281ead2723f7d8f3639c512035abdb
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-12-04 17:01:19 +0100

    Display the flickr photos

--- src/containers/photos/Photos.js
+++ src/containers/photos/Photos.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import './Photos.scss';
+import Flickr from './Flickr.js';
+import { getPhotos } from './actions.js';
 
 class Photos extends Component {
+  componentWillMount() {
+    this.props.getPhotos();
+  }
+
   render() {
     const {
+      photos,
     } = this.props;
 
     return (
-      <div className="photos">
-        hej
+      <div className="photos container">
+        {photos.map(photo => (
+          <img key={photo} src={photo} />
+        ))}
       </div>
     );
   }
 }
 
 export default connect((state) => ({
+  photos: state.photos,
 }), (dispatch) => ({
+  getPhotos: state => dispatch(getPhotos(state)),
 }))(Photos);
