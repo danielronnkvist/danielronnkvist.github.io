28c127ac55b2eb1614ab8d04be1fc6c8f5ea068f
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-12-04 13:29:16 +0100

    Added tabs actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,13 @@ import { requireProject, animateScrollTo } from './helpers.js';
 
 // REDUX ACTION TYPES
 
+export const CHANGE_TAB = 'CHANGE_TAB';
 export const CHANGE_PROJECT = 'CHANGE_PROJECT';
 export const RESET_PROJECT = 'RESET_PROJECT';
 
 // REDUX ACTIONS
+
+export const changeTab = createAction(CHANGE_TAB);
 
 export const changeProject = createAction(CHANGE_PROJECT, (id) => {
   animateScrollTo(500);
