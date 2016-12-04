42380d0f917ff2f25561c498e0eb4d8ba1e58928
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-12-04 19:57:35 +0100

    Close project on esc click

--- src/components/closeButton/CloseButton.js
+++ src/components/closeButton/CloseButton.js
@@ -4,6 +4,32 @@ import { resetProject } from './../../actions';
 import './CloseButton.scss';
 
 class CloseButton extends Component {
+  constructor(props) {
+    super(props);
+    this.close = this.close.bind(this);
+  }
+
+  close(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    } else {
+        isEscape = (evt.keyCode == 27);
+    }
+    if (isEscape) {
+        this.props.resetProject();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.close);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.close);
+  }
+
   render() {
     return (
       <span className="close-button" onClick={this.props.resetProject}></span>
